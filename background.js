a4895b8454c90c7b6460683c23dfb2400087fe98
Niharika Pandit <niharikap219@gmail.com>
2021-07-28 21:06:20 +0530

    feat: dynamic rendering of table rows- DOM manipulation

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
-// let color = '#3aa757';
+let stock = 'ADBE';
+let price = 623;
 
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-//   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ stock });
+  chrome.storage.sync.set({price});
+  console.log('Default stocks set'+ stock);
+});
--- popup.js
+++ popup.js
@@ -22,3 +22,30 @@
 //     document.body.style.backgroundColor = color;
 //   });
 // }
+let color = '#3aaff7';
+let count = 0;
+// Function to change the content of t2
+function myFunction() {
+    var x = document.createElement("TR");
+    x.setAttribute("id", "myTr"+ String(count));
+    document.getElementById("myTable").appendChild(x);
+
+    var y = document.createElement("TD");
+    var z = document.createElement("TD");
+    var r = document.createTextNode("2$");
+    var t = document.createTextNode("TESL");
+    y.appendChild(t);
+    z.appendChild(r);
+    document.getElementById("myTr"+String(count)).appendChild(y);
+    document.getElementById("myTr"+String(count)).appendChild(z);
+    count+=1;
+
+    chrome.storage.local.get(['stock'], function(result) {
+        console.log('Value currently is ' + result.key);
+      });
+
+}
+
+// Add event listener to table
+const el = document.getElementById("add-more");
+el.addEventListener("click", myFunction, false);
